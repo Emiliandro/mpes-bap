68b7ee840f4143a7165df5767cf131e5f2963dfc
emiliandro <emiliandro@live.com>
2023-01-30 23:18:03 -0400

    chore: add cached messages rest api placeholder

--- product/app.py
+++ product/app.py
@@ -1,6 +1,4 @@
 from flask import Flask, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 from utils.RawImportsDB import RawImportsDB as riDB
 from utils.NonRepudiationDB import NonRepudiationDB as nrDB
 from utils.NonRepudiationDB import Nonr
@@ -22,29 +20,65 @@ nr_helper = nrDB()
 # Cached Messages DB contain all messages approved to be sent by the api
 cm_helper = cmDB()
 
+
+# DEMONSTRATION ----------------------------------
+
 raw_imports = ri_helper.getAll()
 non_repudiations = nr_helper.getAll()
 
-#  regarding cyber security non repudiation, this assures that the sender of
+#  Regarding cyber security non repudiation, this assures that the sender of
 #  information is provided with proof of delivery and the recipient is provided
 #  with proof of the sender's request made, so neither can later deny having
 #  processed the information throw the field "Suas ultimas buscas".
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        task_content = request.form['content']
-        new_nr = Nonr(summary=task_content)
-        try:
-            nr_helper.append(new_nr)
-            return redirect('/')
-        except:
-            return 'error adding value'
+        return do_update_nonR(request)
     else:
         raw_imports = ri_helper.getAll()
         non_repudiations = nr_helper.getAll()
 
     title = "Bem vindo ao Bap 🤖"
     return render_template('demonstration.html',title=title, non_repudiations=non_repudiations,raw_imports=raw_imports)
+
+def do_update_nonR(request):
+    task_content = request.form['content']
+    new_nr = Nonr(summary=task_content)
+    try:
+        nr_helper.append(new_nr)
+        return redirect('/')
+    except:
+        return 'error adding value'
+
+# REST API for Cached Messages ---------------------
+@app.route('/get_cached', methods=['GET'])
+def get_all_cached_messages():
+    remove_later = [
+        { 'summary' : 'hello', 'source':'https://google.com' },
+        { 'summary' : 'this', 'source':'https://google.com' },
+        { 'summary' : 'is in', 'source':'https://google.com' },
+        { 'summary' : 'to-do,', 'source':'https://google.com' },
+        { 'summary' : 'bye', 'source':'https://google.com' }]
+    return remove_later
+
+
+@app.route('/get_categorie', methods=['POST', 'GET'])
+def get_categorie_cached_messages():
+    if request.method == 'POST':
+        interest = request.form['content']
+        print(f"user wants {interest}")
+    
+    remove_later = [
+        { 'summary' : 'hello', 'source':'https://google.com' },
+        { 'summary' : 'this', 'source':'https://google.com' },
+        { 'summary' : 'is in', 'source':'https://google.com' },
+        { 'summary' : 'to-do', 'source':'https://google.com' },
+        { 'summary' : 'for the', 'source':'https://google.com' },
+        { 'summary' : 'categories,', 'source':'https://google.com' },
+        { 'summary' : 'bye', 'source':'https://google.com' }]
+    return remove_later
+
+# MISC ---------------------------------------------
 
 if (__name__ == "__main__"):
     app.run(debug=True)

471fa09b3f3b0b827267ea4e149d478dd8e108b2
emiliandro <emiliandro@live.com>
2023-02-21 16:36:13 -0400

    wip scheduler validation

--- product/bap_main.py
+++ product/bap_main.py
@@ -8,26 +8,22 @@ today = datetime.datetime.today()
 today_str = today.strftime('%Y-%m-%d')
 
 class BapMain():
-    def __init__(self):
-        self.file_name = "" + today_str + ".txt"
-        self.valdation = DataHelper()
-        self.webscrapper = BapDDG(today_str)
+    def createTodayFile(self):
         self.webscrapper.makeTxtFile()
 
-        self.current_dictionaries = []
-
-        def get_test_file():
-            try:
-                file = open(self.file_name, 'rb')
-                dictionary_list = pickle.load(file)
-                
-                for d in dictionary_list:
-                    self.current_dictionaries.append(d)
-                file.close()
-            except:
-                print("Something unexpected occurred!")
-
-        get_test_file()
+    def getTodayFile(self):
+        try:
+            file = open(self.file_name, 'rb')
+            dictionary_list = pickle.load(file)
+            
+            for d in dictionary_list:
+                self.current_dictionaries.append(d)
+            file.close()
+        except:
+            print("Something unexpected occurred!")
+    
+    def getAndValidateTodayFiles(self,create_file=False):
+        self.getTodayFile()
         print ("alpaca",len(self.current_dictionaries))
 
         remove_duplicates_current_dictionaries = self.valdation.removeDuplicates(self.current_dictionaries)
@@ -38,5 +34,20 @@ class BapMain():
 
         remove_nonpt_descriptions_dictionaries = self.valdation.removeMinDescription(remove_min_description_dictionaries)
         print ("alpaca",len(remove_nonpt_descriptions_dictionaries))
+
+        if(create_file):
+            with open("validated_"+self.file_name, "wb") as f:
+                pickle.dump(self.list_suggestions, f, protocol=pickle.HIGHEST_PROTOCOL)
+
+        return remove_nonpt_descriptions_dictionaries
+            
+    def __init__(self):
+        self.file_name = "" + today_str + ".txt"
+        self.valdation = DataHelper()
+        self.webscrapper = BapDDG(today_str)
+        self.current_dictionaries = []
+        
+        self.createTodayFile()
+        self.today_files = self.getAndValidateTodayFiles(create_file=True)
 
 start = BapMain()

457d83283273266dba5098013a31aab0cf2b3a70
emiliandro <emiliandro@live.com>
2023-02-25 13:53:01 -0400

    Update category services with crud methods

--- product/category_service.py
+++ product/category_service.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.orm import sessionmaker, declarative_base, load_only
 import copy
 
-
 Base = declarative_base()
 class Category(Base):
     __tablename__ = "saved_categories"
@@ -20,17 +19,42 @@ class CategoryService:
         Session = sessionmaker(bind=engine)
         self.session = Session()
 
-        def delete_category(self,category_id):
-            pass
+    def delete_category(self,category_id):
+        message = self.session.query(Category).get(category_id)
+        if message is None:
+            raise ValueError("Message not found")
+
+        self.session.delete(message)
+        self.session.commit()
+        return {'message': 'Message deleted successfully'}
         
-        def update_category(self,category_id, categorie_name):
-            pass
+    def update_category(self,category_id, categorie_name):
+        message = self.session.query(Category).get(category_id)
+        if message is None:
+            raise ValueError("Message not found")
 
-        def create_categorys(self,categories):
-            pass
+        message.name = categorie_name
+        self.session.commit()
+        return self._message_to_dict(message)
 
-        def create_category(self,categorie_name):
-            pass
+    def create_categorys(self,categories):
+        for msg in categories:
+            message = self.prototype.clone()
+            message.name = msg['name']
+            self.session.add(message)
+        self.session.commit()
+        return {'response':'added'}
 
-        def get_all_categorys(self):
-            pass
+    def create_category(self,categorie_name):
+        message = self.prototype.clone()
+        message.name = categorie_name
+        self.session.add(message)
+        self.session.commit()
+        return self._message_to_dict(message)
+
+    def get_all_categorys(self):
+        messages = self.session.query(Category).all()
+        return [self._message_to_dict(message) for message in messages]
+
+    def _message_to_dict(self, category):
+        return { 'id': category.id, 'category': category.name, }

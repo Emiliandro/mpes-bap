4b28c566439bacf5ca9d2e555dd769dd58a59965
emiliandro <emiliandro@live.com>
2023-03-06 10:19:56 -0400

    Add validation of url to data helper

--- product/data_helper.py
+++ product/data_helper.py
@@ -1,4 +1,6 @@
 from langdetect import detect
+from urllib.request import urlopen
+from urllib.error import URLError, HTTPError
 
 class DataHelper(object):
     def removeNotPortuguese(self,messages):
@@ -14,3 +16,16 @@ class DataHelper(object):
 
     def removeDuplicates(self, messages):
         return [dict(t) for t in {tuple(d.items()) for d in messages if "source" in d and not any(d["source"] == x["source"] for x in messages if x is not d)}]
+
+    def validateUrl(self,url):
+        try:
+            response = urlopen(url)
+            print("The URL exists")
+        except HTTPError as e:
+            print("The URL does not exist (HTTP Error)")
+            return False
+        except URLError as e:
+            print("The URL does not exist (URL Error)")
+            return False
+        
+        return True

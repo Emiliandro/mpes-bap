62e4ff90d64859968b783cf3b20419102a094301
emiliandro <emiliandro@live.com>
2023-03-08 00:15:32 -0400

    [Improvements] refactoring bap_main

--- product/bap_main.py
+++ product/bap_main.py
@@ -1,8 +1,15 @@
-from data_helper import DataHelper
-from bap_ddg import BapDDG
-from abc import ABC, abstractmethod
 import datetime
-import pickle
+
+# using duckduckgo_search, because DuckDuckGo does not collect or share personal
+# information. That its privacy policy, it prevents search leakage by default.  
+# read more about it at https://duckduckgo.com/privacy
+from duckduckgo_search import ddg_suggestions, ddg
+
+# escape function causes param to be rendered as text, preventing the execution of 
+# injection script in the user’s browser or the in the api request.
+from markupsafe import escape
+from abc import ABC, abstractmethod
+from data_helper import DataHelper
 
 today = datetime.datetime.today()
 today_str = today.strftime('%Y-%m-%d')
@@ -47,19 +54,12 @@ class HandlerRemoveNonPortuguese(Handler):
         return data
 
 class HandlerCreateFile(Handler):
-    def __init__(self, create_file=False):
-        self.create_file = create_file
-        self.file_name = "" + today_str + ".txt"
-
     def handle(self, data):
-        if self.create_file:
-            with open("validated_"+self.file_name, "wb") as f:
-                pickle.dump(data, f, protocol=pickle.HIGHEST_PROTOCOL)
         return data
 
 class BapMain():
     def createTodayFile(self):
-        self.webscrapper.make_txt_file()
+        self.research = self.webscrapper.research()
 
     def set_categories(self, categories):
         list_categories = []
@@ -68,22 +68,16 @@ class BapMain():
         self.webscrapper.set_categories(list_categories)
 
     def getTodayFile(self):
-        try:
-            file = open(self.file_name, 'rb')
-            dictionary_list = pickle.load(file)
-            
-            for d in dictionary_list:
-                self.current_dictionaries.append(d)
-            file.close()
-        except:
-            print("Something unexpected occurred!")
+        dictionary_list = self.research
+        for d in dictionary_list:
+            self.current_dictionaries.append(d)
     
-    def getAndValidateTodayFiles(self, create_file=False):
+    def getAndValidateTodayFiles(self):
         self.getTodayFile()
         print("initial size of scrapping", len(self.current_dictionaries))
 
         # Chain of responsibility
-        handler_create_file = HandlerCreateFile(create_file)
+        handler_create_file = HandlerCreateFile()
         handler_remove_non_portuguese = HandlerRemoveNonPortuguese(handler_create_file)
         handler_remove_min_description = HandlerRemoveMinDescription(handler_remove_non_portuguese)
         handler_remove_duplicates = HandlerRemoveDuplicates(handler_remove_min_description)
@@ -94,9 +88,48 @@ class BapMain():
     def __init__(self):
         self.file_name = today_str + ".txt"
         self.webscrapper = BapDDG(today_str)
+        self.research = []
         self.current_dictionaries = []
         
     def get_messages(self):
         self.createTodayFile()
-        self.today_files = self.getAndValidateTodayFiles(create_file=True)
+        self.today_files = self.getAndValidateTodayFiles()
         return self.today_files
+
+class BapDDG():
+    def set_categories(self,categories):
+        self.categories = categories
+
+    def search_phrase(self,value,category='none'):
+        self.phrase_to_results = ddg(value, region='br-pt', safesearch='on', time='y', max_results=9)
+
+        for phrase in self.phrase_to_results:
+            self.list_suggestions.append({ 
+                "description":str(phrase['body']),
+                "title":str(phrase['title']),
+                "source":str(phrase['href']),
+                "category":str(category)})
+
+    def __init__(self,today):
+        self.default_turismo = [" turismo sustentável", "turismo g1"]
+        self.default_tecnologia = [" tecnologia "," tecnologia da informação "," tecnologia assistiva "," tecnologia 5g "," tecnologia na educação "," tecnologia sinônimo "," tecnologias disruptivas "," tecnologia e inovação "," tecnologia da informação "," tecnologia da informação faculdade "," tecnologia da informação e comunicação ", " tecnologia g1"]
+        self.default_automobilismo = [" autoesporte "," autoesporte g1 "," auto esporte globo "," autoesporte domingo "," autoesporte passo fundo "," autoesporte qual comprar 2022 "," autoesporte mclaren "," auto esporte revista "," autoesporte g1 "," auto esporte globo "," auto esporte globoplay "," auto esporte globo domingo "," autoesporte domingo "," auto esporte revista "," autosport revista "," autoesporte g1 "," auto esporte globo "," auto esporte globoplay "," auto esporte globo domingo "," auto esporte globoplay "," auto esporte globo domingo "," auto esporte de hoje domingo "," auto esporte globo hoje "," auto esporte de domingo "," auto esporte na globo "," auto esporte globo ontem "," programa auto esporte globo "," auto esporte globo play "," autoesporte domingo "," autoesporte passo fundo "," auto esporte passo fundo telefone "," auto esporte passo fundo rs "," auto esporte passo fundo centro "," autoesporte mclaren "," autosport mclaren f1 forum "," autosport mclaren f1 forum "," automotive forums mclaren f1 "]
+        self.list_suggestions = []
+        self.file_name = "" + today + ".txt"
+        self.categories = []
+
+    def research(self):
+
+        for i in self.categories:
+            self.search_phrase(value=i,category=i)
+
+        for i in self.default_automobilismo:
+            self.search_phrase(value=i,category="autoesporte")
+
+        for i in self.default_tecnologia:
+            self.search_phrase(value=i,category="tecnologia")
+            
+        for i in self.default_turismo:
+            self.search_phrase(value=i,category="turismo")
+        
+        return self.list_suggestions
